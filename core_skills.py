33bac62b14f4ea1f6908ac5956563468f812b150
Arif <arif@welabs.dev>
2026-02-13 00:02:12 +0600

    #1 Python Core Skills portion has been done.

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,22 @@
 import random
-# rand_list =
+rand_list = [random.randint(1, 20) for _ in range(10)]
 
-# list_comprehension_below_10 =
+print(rand_list)
 
-# list_comprehension_below_10 =
+# showing result using comprehension
+list_comprehension_below_10 = [ i for i in rand_list if i < 10 ]
+
+print(list_comprehension_below_10)
+
+
+#  showing results using filter
+def less_than_10(num):
+    if num < 10:
+        return num
+    else:
+        False
+        
+list_comprehension_below_10_filter = filter(less_than_10, rand_list)
+
+result_list = [i for i in list_comprehension_below_10_filter]
+print(result_list)

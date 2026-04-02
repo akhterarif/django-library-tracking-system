54458f436777a543dbefe199d5413c196ef8d3c8
akhterarif <arif.cse28@gmail.com>
2026-04-02 22:10:16 +0600

    Added the core-skils problem-solution

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,15 @@
 import random
-# rand_list =
+rand_list = [random.randint(1, 20) for i in range(20)]
+print(rand_list)
+list_comprehension_below_10 = [i for i in rand_list if i < 10]
 
-# list_comprehension_below_10 =
+print(list_comprehension_below_10)
 
-# list_comprehension_below_10 =
+def below_10(num):
+    if num< 10:
+        return num
+
+filt_list_comprehension_below_10 = filter(below_10, rand_list)
+list_comprehension_below_10 = [i for i in filt_list_comprehension_below_10]
+
+print(list_comprehension_below_10)
